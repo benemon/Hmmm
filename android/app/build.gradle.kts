f98c8451cba41134647caae218afca9caf22f5ad
Benjamin Holmes <benjamin.holmes@gmail.com>
2026-09-09 20:58:16 +0100

    build: application id cloud.onmi.hmmm and versioned APK name (v0.3.0)
    - Android namespace, application id, and Kotlin package move from
      home.orbital.hmmm to cloud.onmi.hmmm; iOS bundle identifiers follow.
      Existing installs are a different app to the system, consistent with the
      clean-slate schema policy.
    - Gradle archive name is hmmm-<version>, so the release APK is
      hmmm-0.3.0-release.apk; CI uploads it as a workflow artifact.
    - Version 0.3.0+7.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "home.orbital.hmmm"
+    namespace = "cloud.onmi.hmmm"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -15,8 +15,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "home.orbital.hmmm"
+        applicationId = "cloud.onmi.hmmm"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -36,6 +35,10 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+base {
+    archivesName = "hmmm-${flutter.versionName}"
 }
 
 kotlin {
